05da4f48b80f331b6a4d41ab065ae3d3bd47f0de
Jared Smerdell <jared.smerdell@western.edu>
2021-04-08 14:51:09 -0600

    test again

---
+++ src/Grid.java
@@ -0,0 +1,10 @@
+import java.util.Vector;
+
+public class Grid {
+    static int size = 8;
+
+    public Vector<Vector<Integer>> createGrid() {
+        Vector<Vector<Integer>> grid = new Vector<Vector<Integer>>();
+        return grid;
+    }
+}
