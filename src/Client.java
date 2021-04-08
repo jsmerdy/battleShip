756c92411a15b2417d1011e969a6a8b63c1341aa
Jared Smerdell <jared.smerdell@western.edu>
2021-04-08 15:13:04 -0600

    added grid

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,6 @@
 public class Client {
     public static void main(String[] args) {
-        System.out.println("test");
-
-        System.out.println("test2");
+        Grid grid = new Grid();
+        grid.printGrid();
     }
 }
