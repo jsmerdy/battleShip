48d7ddb5006adc3a110d7478533176b270718470
Jared Smerdell <jared.smerdell@western.edu>
2021-04-08 19:41:24 -0600

    Refined networking

--- src/Grid.java
+++ src/Grid.java
@@ -10,7 +10,6 @@ public class Grid {
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
                 grid[i][j] = value;
-                value++;
             }
         }
     }
